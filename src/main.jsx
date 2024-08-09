97f6e1fa5e33ef7987e58e94d7eccb6264a63099
Myo Myat Min <myomyatmin315@gmail.com>
2024-08-10 01:10:49 +0700

    c Add index route for homepage

--- src/main.jsx
+++ src/main.jsx
@@ -10,6 +10,7 @@ import ErrorPage from "./error-page";
 import Contact, { loader as contactLoader } from "./routes/contact";
 import EditContact, { action as editAction } from "./routes/edit";
 import { action as destroyAction } from "./routes/destroy";
+import Index from "./routes";
 const router = createBrowserRouter([
   {
     path: "/",
@@ -18,6 +19,10 @@ const router = createBrowserRouter([
     loader: rootLoader,
     action: rootAction,
     children: [
+      {
+        index: true,
+        element: <Index />,
+      },
       {
         path: "/contacts/:contactId",
         element: <Contact />,
@@ -32,6 +37,7 @@ const router = createBrowserRouter([
       {
         path: "/contacts/:contactId/destroy",
         action: destroyAction,
+        errorElement: <div>Oops! There was an error.</div>,
       },
     ],
   },
